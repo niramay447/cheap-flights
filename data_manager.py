bed413135af7d4736a286c0d1de23efcb03c2b9f
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-06-18 13:05:39 +0530

    getting data from sheety comparing iata updating iata

--- data_manager.py
+++ data_manager.py
@@ -1,16 +1,30 @@
 import requests
 
-SHEETY_ENDPOINT = "https://api.sheety.co/cf63aa3d1cb6c7ae5ecf49eb6a340d82/flightDeals/prices"
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/cf63aa3d1cb6c7ae5ecf49eb6a340d82/flightDeals/prices"
+
 
 class DataManager:
 
-    def __int__(self):
-        self.destination_code = {}
+    def __init__(self):
+        self.destination_data = {}
 
     def get_destination_data(self):
-
-        response = requests.get(SHEETY_ENDPOINT)
+        # 2. Use the Sheety API to GET all the data in that sheet and print it out.
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
         data = response.json()
-        self.destination_code = data["prices"]
+        self.destination_data = data["prices"]
 
-        return self.destination_code
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
