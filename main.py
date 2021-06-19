a198cf510ee2a8f20e4fff47827645ee8c341cf0
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-06-19 15:52:32 +0530

    get IATA codes

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import datetime
 
 from data_manager import DataManager
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
 
+ORIGIN_CITY_IATA = "AMD"
 if sheet_data[0]["iataCode"] == "":
     from flight_search import FlightSearch
     flight_search = FlightSearch()
@@ -12,3 +14,14 @@ if sheet_data[0]["iataCode"] == "":
 
     data_manager.destination_data = sheet_data
     data_manager.update_destination_codes()
+
+tomorrow = datetime.now() + datetime.timedelta(days=1)
+six_months_from_today = datetime.now() + datetime.timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time = tomorrow,
+        to_time = six_months_from_today
+    )
